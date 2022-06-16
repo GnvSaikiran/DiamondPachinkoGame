73d61ca0179af27178fab58f18df3f23350210d4
Gnv Saikiran <61322778+GnvSaikiran@users.noreply.github.com>
2022-06-17 03:15:40 +0530

    initial commit

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * (Time.deltaTime * 12);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * (Time.deltaTime * 12);
+        }
+    }
+}
